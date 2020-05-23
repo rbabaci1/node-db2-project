30db6883ec716c4e3408902bd0093059e4725b69
rabah babaci <4rabah@gmail.com>
2020-05-23 11:04:24 -0700

    create and test the cars router

--- api/server.js
+++ api/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const helmet = require("helmet");
+
 const db = require("../data/dbConfig");
+const carsRouter = require("../api/carsRouter");
 
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use("/api/cars", carsRouter);
 
 server.get("/api/", (req, res) => {
   res.status(200).json({ message: "API is up!!!" });
