daffcfd4091e15a9129c3cb0ec3f4c89ad2c18b8
rabah babaci <4rabah@gmail.com>
2020-05-23 10:22:47 -0700

    create the vehicles seeds

--- data/migrations/20200523101436_create-cars-table.js
+++ data/migrations/20200523101436_create-cars-table.js
@@ -1,14 +1,14 @@
 exports.up = function (knex) {
   return knex.schema.createTable("cars", table => {
-    table.increments();
+    table.increments("car_id");
     // required table columns
-    table.string("VIN", 128).notNullable().unique();
+    table.string("VIN", 128).unique().notNullable();
     table.string("make", 128).notNullable();
     table.string("model", 128).notNullable();
     table.integer("mileage").notNullable();
-    // optional columns
-    table.string("transmission", 128);
+    // optional table columns
     table.string("title", 128);
+    table.string("status", 128);
   });
 };
 
