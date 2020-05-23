921465d613d7d127027cd6a6ed8359149150e795
rabah babaci <4rabah@gmail.com>
2020-05-23 10:40:26 -0700

    run all the seeds

--- data/seeds/01-vehicles.js
+++ data/seeds/01-vehicles.js
@@ -1,13 +1,9 @@
+const initialData = require("../../mockData");
+
 exports.seed = function (knex) {
-  // Deletes ALL existing entries
   return knex("cars")
     .del()
     .then(function () {
-      // Inserts seed entries
-      return knex("cars").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" },
-      ]);
+      return knex("cars").insert(initialData);
     });
 };
