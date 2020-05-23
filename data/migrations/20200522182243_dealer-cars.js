3ae30ac7a2c29d69b89a3b800dfbd2edc00a0fe3
rabah babaci <4rabah@gmail.com>
2020-05-22 19:09:38 -0700

    update database columns form text to string

--- data/migrations/20200522182243_dealer-cars.js
+++ data/migrations/20200522182243_dealer-cars.js
@@ -2,13 +2,13 @@ exports.up = function (knex) {
   return knex.schema.createTable("cars", table => {
     table.increments();
     // required table columns
-    table.text("VIN", 128).unique().notNullable();
-    table.text("make", 128).notNullable();
-    table.text("model", 128).notNullable();
+    table.string("VIN", 128).notNullable().unique();
+    table.string("make", 128).notNullable();
+    table.string("model", 128).notNullable();
     table.integer("mileage").notNullable();
     // optional columns
-    table.text("transmission", 128);
-    table.text("title", 128);
+    table.string("transmission", 128);
+    table.string("title", 128);
   });
 };
 
