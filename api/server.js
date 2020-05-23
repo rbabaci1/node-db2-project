b06cba0763c5045fc9481986c936272747fbe7d2
rabah babaci <4rabah@gmail.com>
2020-05-23 11:07:54 -0700

    add a errorHandler middleware to all routes

--- api/server.js
+++ api/server.js
@@ -10,8 +10,16 @@ server.use(express.json());
 server.use(helmet());
 server.use("/api/cars", carsRouter);
 
-server.get("/api/", (req, res) => {
+server.get("/", (req, res) => {
   res.status(200).json({ message: "API is up!!!" });
 });
+
+const errorHandler = (error, req, res, next) => {
+  const code = error.status || error.statusCode || 500;
+
+  res.status(code).json(error);
+};
+
+server.use(errorHandler);
 
 module.exports = server;
