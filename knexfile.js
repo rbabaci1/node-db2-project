a62966c88cafd2fe7a70682e936614fb69882109
rabah babaci <4rabah@gmail.com>
2020-05-22 18:54:55 -0700

    add a configuration for the seeds

--- knexfile.js
+++ knexfile.js
@@ -4,9 +4,12 @@ module.exports = {
     connection: {
       filename: "./data/car-dealer.db3",
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations",
     },
-    useNullAsDefault: true,
+    seeds: {
+      directory: "./data/seeds",
+    },
   },
 };
