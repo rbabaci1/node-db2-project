1384ea3c4e11b88faa94f0c6de6868bb20e431a0
rabah babaci <4rabah@gmail.com>
2020-05-22 18:36:14 -0700

    in the migration up function, create the database schema

--- data/migrations/20200522182243_dealer-cars.js
+++ data/migrations/20200522182243_dealer-cars.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", table => {
+    table.increments();
+    // required table columns
+    table.text("VIN", 128).unique().notNullable();
+    table.text("make", 128).notNullable();
+    table.text("model", 128).notNullable();
+    table.integer("mileage").notNullable();
+    // optional columns
+    table.text("transmission", 128);
+    table.text("title", 128);
+  });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
