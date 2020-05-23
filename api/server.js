f8ac8522f13f93f2610bb0ce731613fd712e7190
rabah babaci <4rabah@gmail.com>
2020-05-22 18:21:20 -0700

    add the migrations config object

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("../data/dbConfig");
 
 const server = express();
 
--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,11 @@ module.exports = {
   development: {
     client: "sqlite3",
     connection: {
-      filename: "./dev.sqlite3",
+      filename: "./data/car-dealer.db3",
     },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    useNullAsDefault: true,
   },
 };
