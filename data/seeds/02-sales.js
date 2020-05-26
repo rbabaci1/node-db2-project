c382af63138bd540e67eb6b6bcbc456d735b17c7
rabah babaci <4rabah@gmail.com>
2020-05-25 19:20:18 -0700

    add mock data to the sales table

--- data/seeds/02-sales.js
+++ data/seeds/02-sales.js
@@ -1,13 +1,20 @@
-
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex("sales")
+    .del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("sales").insert([
+        { car_id: 1, sales_amount: "$1999" },
+        { car_id: 1, sales_amount: "$1999" },
+        { car_id: 1, sales_amount: "$1999" },
+        { car_id: 3, sales_amount: "$3999" },
+        { car_id: 3, sales_amount: "$3999" },
+        { car_id: 3, sales_amount: "$3999" },
+        { car_id: 4, sales_amount: "$4999" },
+        { car_id: 5, sales_amount: "$5999" },
+        { car_id: 6, sales_amount: "$6999" },
+        { car_id: 7, sales_amount: "$7999" },
       ]);
     });
 };
